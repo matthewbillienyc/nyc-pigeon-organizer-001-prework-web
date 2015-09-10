02c9d3e9414ec7da1aa2e9dc5ba95f1ddd064f51
matthewbillienyc <matthewbillienyc@gmail.com>
2015-09-10 13:27:48 -0400

    passing

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons_by_name = {}
+
+  data.each do |trait, datas|
+    datas.each do |datas2, name|
+      name.each do |name|
+        if pigeons_by_name.has_key?(name) == false
+          pigeons_by_name[name]= {trait=>[]}
+          pigeons_by_name[name][trait].push(datas2.to_s)
+        else
+          if pigeons_by_name[name].has_key?(trait) == true
+            pigeons_by_name[name][trait].push(datas2.to_s)
+          else
+            pigeons_by_name[name][trait]= []
+            pigeons_by_name[name][trait].push(datas2.to_s)
+          end
+        end
+      end
+    end
+  end
+  pigeons_by_name
 end
